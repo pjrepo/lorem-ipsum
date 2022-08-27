b902fd38d2a780b37e98d1668dde6d811a4cded8
pjrepo <pavanjegurupati75@gmail.com>
2022-08-27 07:17:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./Data";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const clickHandler = (event) => {
+    event.preventDefault();
+    let amount = parseInt(count);
+    if (count < 1) setText(["select a valid number"]);
+    else if (count > data.length)
+      setText([
+        "You have exceeded the maximum number of paragraphs, displaying all the existing paragraphs",
+        ...data.slice(0, data.length),
+      ]);
+    else setText(data.slice(0, amount));
+    setCount(0);
+  };
+
+  const changeHandler = (event) => {
+    event.preventDefault();
+    setCount(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <h3>tired of boring lorem ipsum</h3>
+        <form>
+          <label htmlFor="amount">paragraphs:</label>
+          <input type="number" value={count} onChange={changeHandler} />
+          <button onClick={clickHandler}>Generate</button>
+        </form>
+        <article>
+          {text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
+        </article>
+      </section>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
